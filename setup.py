af47d426f72d81d7c12bf3549106c5e8a0ce8571
Amirali Esfandiari <amiralinull@gmail.com>
2018-11-02 18:01:54 +0330

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kutt',
-    version='1.0',
+    version='1.1',
     py_modules=['kutt'],
     install_requires = ['click', 'requests'],
     entry_points = '''
